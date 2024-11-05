41fe89db1789c6df202ff56115b9e55107fd4a4c
Kimonoduck <kimonoduck@gmail.com>
2024-11-05 15:48:35 +0900

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,3 +119,27 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media/"
+
+INSTALLED_APPS = [
+#...
+'blog’,
+'rest_framework',
+]
+#…
+REST_FRAMEWORK = {
+'DEFAULT_PERMISSION_CLASSES': [
+# 'rest_framework.permissions.IsAdminUser’,
+],
+'PAGE_SIZE': 10
+}
+
+INSTALLED_APPS = [
+#...
+'blog’,
+'rest_framework’,
+ 'rest_framework.authtoken', #added
+]
--- mysite/urls.py
+++ mysite/urls.py
@@ -2,8 +2,14 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
-
+from django.conf.urls.static import static
+from rest_framework.authtoken.views import obtain_auth_token #added
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('blog.urls')),
+path('admin/', admin.site.urls), #a)
+path('', include('blog.urls')),
+path('api-token-auth/', obtain_auth_token), #added
 ]
+
+
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
